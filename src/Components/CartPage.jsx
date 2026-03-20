08100cb140c78e231c3403be5a24ee9ccb2216fd
RGPin <157803400+RGPin@users.noreply.github.com>
2026-03-21 07:03:06 +0800

    Set up Link...sss

--- src/Components/CartPage.jsx
+++ src/Components/CartPage.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router";
 
-
-function CartPage () {
-    return <h1>Cart Page</h1>
+function CartPage() {
+  return (
+    <>
+      <h1>Cart Page</h1>
+      <Link to="/">Home</Link>
+      <Link to="/shop">Shop</Link>
+      <Link to="/cart">Cart</Link>
+    </>
+  );
 }
 
 export default CartPage;
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router";
 
-
-function HomePage () {
-    return <h1>Home page</h1>
+function HomePage() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <Link to="/">Home</Link>
+      <Link to="/shop">Shop</Link>
+      <Link to="/cart">Cart</Link>
+    </>
+  );
 }
 
 export default HomePage;
