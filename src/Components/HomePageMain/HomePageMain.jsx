4c95bb456f3730c031d971c2e30858cff0080b7e
RGPin <157803400+RGPin@users.noreply.github.com>
2026-04-01 06:20:28 +0800

    Add homepage main

---
+++ src/Components/HomePageMain/HomePageMain.jsx
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+function HomePageMain() {
+  return (
+    <main className="home-main">
+      <div className="about">
+        <h1>Cartly</h1>
+        <h3>Shopping Cart uhhhh app. Yes.</h3>
+        <p>
+          Text here. More text here. This is a text. This text was placed here
+          to add some text cause I needed some text here and I don't know what
+          other text to place here. Hence, this text is here.
+        </p>
+      </div>
+      <div className="img-container">
+        <img src="/assets/main.png" />
+      </div>
+    </main>
+  );
+}
+
+export default HomePageMain;
--- src/Components/pages/HomePage.jsx
+++ src/Components/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router";
 import Header from "../Header";
+import HomePageMain from "../HomePageMain/HomePageMain";
 
 function HomePage() {
   return (
     <>
       <Header />
+      <HomePageMain />
     </>
   );
 }
