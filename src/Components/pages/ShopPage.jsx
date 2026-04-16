0d6b7a69ae6f2ce69304d15ba98756c7fdd39210
RGPin <157803400+RGPin@users.noreply.github.com>
2026-04-17 07:40:40 +0800

    useFetch hook

--- src/Components/pages/ShopPage.jsx
+++ src/Components/pages/ShopPage.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import useFetch from "../../hooks/useFetch";
 
 function ShopPage() {
+  const [data, error, loading] = useFetch(
+    "https://fakestoreapi.com/products",
+    {},
+  );
+  console.log(data);
   return (
     <>
       <Header />
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(url, options = {}) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function fetchData() {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const response = await fetch(url, {
+          ...options,
+          signal: controller.signal,
+        });
+
+        if (!response.ok) throw new Error(response.statusText);
+
+        const data = await response.json();
+        setData(data);
+      } catch (e) {
+        if (e.name !== "AbortError") {
+          setError(e);
+          console.error(e);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+
+    return () => controller.abort();
+  }, [url]);
+
+  return [data, error, loading];
+}
